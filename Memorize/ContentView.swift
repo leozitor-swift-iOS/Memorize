08de713173f8e09120575f35ff5a4ba1a2de72b2
Leozitor Floro de Souza <leozitor@gmail.com>
2020-06-25 18:21:28 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Leozítor Floro on 25/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leozítor Floro on 25/06/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

5b7e049ae027eb8fa67955144d3da28974771f51
Leozitor Floro de Souza <leozitor@gmail.com>
2020-07-17 05:40:06 -0400

    Game finished as the course asks for.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,23 +14,27 @@ struct EmojiMemoryGameView: View {
             Text(viewModel.theme.name).font(Font.largeTitle.bold()).padding(.top)
             Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    viewModel.choose(card: card)
+                    withAnimation(.linear(duration: 0.75)) {
+                        viewModel.choose(card: card)
+                    }
                 }
                 .padding(5)
             }
             .padding()
             .foregroundColor(viewModel.theme.color)
-            HStack(){
-                Text("Score: \(viewModel.score) ")
-                    .font(Font.largeTitle.bold()).padding(.leading, 25.0)
+            HStack {
+                Text("Score: \(viewModel.score) ").font(Font.largeTitle.bold()).padding(.leading, 25.0)
                 Spacer()
-                Button(action: viewModel.resetGame) {
-                    Text("New Game").font(Font.title.italic())
+                Button(action: {
+                    withAnimation(.easeInOut) { viewModel.resetGame() }
+                }, label: {
+                    Text("New Game")
+                        .font(Font.title.italic())
                         .padding()
                         .foregroundColor(.white)
                         .background(Color.blue)
                         .cornerRadius(10.0)
-                }.padding(.trailing, 25.0)
+                }).padding(.trailing, 25.0)
             }
         }
     }
@@ -45,16 +49,37 @@ struct CardView: View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched{
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                    .padding(5).opacity(0.4)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                            .onAppear {
+                                startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                    }
+                }
+                .padding(5).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 :0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
     }
 }
@@ -65,7 +90,6 @@ private func fontSize(for size: CGSize) -> CGFloat {
     min(size.width, size.height) * 0.7
     
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
